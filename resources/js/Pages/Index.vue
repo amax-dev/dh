d4640aee0c8bf5db8d72dcd11f1741f754e7f99f
admir <admir@adrovic.net>
2023-04-19 01:43:59 +0200

    Naslovna postavljanje

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-col min-w-full min-h-screen items-center justify-center">
+    <div>
+        <img :src="'storage/img/logo.jpg'">
+    </div>
+
+    <div class="mt-3 text-md font-semibold hover:font-medium">
+        <Link :href="route('login')">Prijavi se</Link>
+    </div>
+
+    </div>
+</template>
+
+<script>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+export default {
+    components:{
+        Link, Head
+    }
+
+}
+</script>
